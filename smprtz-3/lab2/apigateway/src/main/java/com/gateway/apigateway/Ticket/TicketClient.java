85409067b5109b0b99a64001b8460701e851f457
Виктория <31704071+youngdecadent@users.noreply.github.com>
2020-02-18 02:20:34 +0200

    gateway

---
+++ smprtz-3/lab2/apigateway/src/main/java/com/gateway/apigateway/Ticket/TicketClient.java
@@ -0,0 +1,47 @@
+package com.gateway.apigateway.Ticket;
+
+import com.gateway.apigateway.GetResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.Optional;
+
+@FeignClient(value = "ticket-service")
+@RequestMapping(path="/ticket")
+public interface TicketClient {
+
+    @RequestMapping(path = "/list", method = RequestMethod.GET)
+    public @ResponseBody
+    GetResponse<Iterable<Ticket>> getAll();
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    GetResponse<Optional<Ticket>> getById(@PathVariable int id);
+
+    @RequestMapping(path = "/find/{event}", method = RequestMethod.GET)
+    public @ResponseBody
+    GetResponse<Ticket> getByEvent(@PathVariable String event);
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public @ResponseBody
+    String add(@RequestParam String event,
+               @RequestParam String date,
+               @RequestParam String location,
+               @RequestParam String zone,
+               @RequestParam Integer place,
+               @RequestParam Float price) throws ParseException;
+
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    public @ResponseBody
+    void delete(@PathVariable Integer id);
+
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    public @ResponseBody
+    String update(@RequestParam String event,
+                  @RequestParam String date,
+                  @RequestParam String location,
+                  @RequestParam String zone,
+                  @RequestParam Integer place,
+                  @RequestParam Float price) throws ParseException;
+}
